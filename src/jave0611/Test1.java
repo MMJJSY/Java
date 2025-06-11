0389fb18e31271387345b6cf3d8d0afcd2eab361
MMJJSY <hur6918@gmail.com>
2025-06-11 17:24:09 +0900

    Update Test1.java

--- src/jave0611/Test1.java
+++ src/jave0611/Test1.java
@@ -4,5 +4,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.print("A");
+        System.out.println("B");
+        System.out.print("C");
     }
 }
