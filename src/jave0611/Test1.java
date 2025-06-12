8217ac3511319c01e20a50661b683728aba7f9d3
MMJJSY <hur6918@gmail.com>
2025-06-12 15:03:18 +0900

    ㅇ

--- src/jave0611/Test1.java
+++ src/jave0611/Test1.java
@@ -2,6 +2,7 @@ package jave0611;
 
 public class Test1 {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
         System.out.println("Hi");
         System.out.print("A");
